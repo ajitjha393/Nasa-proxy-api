1b5ed639135e192b27006ab7d8e89680e122b18a
Bishwajit Jha <ajitjha393@gmail.com>
2021-04-06 10:35:57 +0530

    Added proxy server route for hiding api key

--- src/api/mars-weather.js
+++ src/api/mars-weather.js
@@ -1,23 +1,28 @@
 const express = require('express');
+const axios = require('axios');
 
 const router = express.Router();
 
 const BASE_URL = 'https://api.nasa.gov/insight_weather/?';
 
 // Will be our proxy api to Nasa
-router.get('/', (req, res) => {
-	const params = new URLSearchParams({
-		api_key: process.env.NASA_API_KEY,
-		feedtype: 'json',
-		ver: '1.0',
-	});
+router.get('/', async (req, res, next) => {
+	try {
+		const params = new URLSearchParams({
+			api_key: process.env.NASA_API_KEY,
+			feedtype: 'json',
+			ver: '1.0',
+		});
 
-	//1. Make a req to Nasa Endpt from here
-	//2. Then Respond to this req by Data received from Nasa
+		//1. Make a req to Nasa Endpt from here
 
-	res.json({
-		message: 'Hello Mars weather!',
-	});
+		const { data } = await axios.get(`${BASE_URL}${params}`);
+		//2. Then Respond to this req by Data received from Nasa
+
+		res.json(data);
+	} catch (err) {
+		next(err);
+	}
 });
 
 module.exports = router;
