ca28b79eedea22b92c2f1388d0a393158f748192
Bishwajit Jha <ajitjha393@gmail.com>
2021-04-06 10:12:40 +0530

    Added mars-weather route

--- src/api/index.js
+++ src/api/index.js
@@ -2,14 +2,16 @@ const express = require('express');
 
 const emojis = require('./emojis');
 
+const marsWeather = require('./mars-weather');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json({
-    message: 'API - 👋🌎🌍🌏'
-  });
+	res.json({
+		message: 'API - 👋🌎🌍🌏',
+	});
 });
 
 router.use('/emojis', emojis);
+router.use('/mars-weather', marsWeather);
 
 module.exports = router;
