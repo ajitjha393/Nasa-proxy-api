fce61787e0cb88aba810a77710b4d2180018299e
Bishwajit Jha <ajitjha393@gmail.com>
2021-04-06 10:21:25 +0530

    Added Base url and params

--- src/api/mars-weather.js
+++ src/api/mars-weather.js
@@ -2,8 +2,22 @@ const express = require('express');
 
 const router = express.Router();
 
+const BASE_URL = 'https://api.nasa.gov/insight_weather/?';
+
+// Will be our proxy api to Nasa
 router.get('/', (req, res) => {
-	res.json(['😀', '😳', '🙄', '🤣']);
+	const params = new URLSearchParams({
+		api_key: 'DEMO_KEY',
+		feedtype: 'json',
+		ver: '1.0',
+	});
+
+	//1. Make a req to Nasa Endpt from here
+	//2. Then Respond to this req by Data received from Nasa
+
+	res.json({
+		message: 'Hello Mars weather!',
+	});
 });
 
 module.exports = router;
